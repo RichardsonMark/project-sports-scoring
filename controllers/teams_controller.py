4418926a794ce6d902d9a66b5fb21ab90c351c84
Mark Richardson <mark_richardson@sky.com>
2020-11-29 11:44:14 +0000

    added Blueprint to teams_controller

---
+++ controllers/teams_controller.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, Flask, redirect, render_template, request
+
+from models.team import Team
+import repositories.team_repository as team_repository
+
+teams_blueprint = Blueprint("teams", __name__)
+
+# INDEX
+@teams_blueprint.route("/clubs")
+def teams():
+#    teams = team_repository.select_all()
+    return render_template("clubs/index.html", teams=teams)
