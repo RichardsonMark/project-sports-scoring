74ebcb9a1f9bf88326781156df8eb1e81d511687
Mark Richardson <mark_richardson@sky.com>
2020-11-30 17:04:16 +0000

    adding 1st version of create/edit/delete player, create and delete fully working

--- controllers/players_controller.py
+++ controllers/players_controller.py
@@ -11,3 +11,44 @@ def players():
     players = player_repository.select_all()
     return render_template("clubs/players/index.html", players=players)
 
+
+# NEW
+@players_blueprint.route("/clubs/players/new")
+def new_player():
+    return render_template("clubs/players/new.html")
+
+
+# CREATE
+@players_blueprint.route("/clubs/players", methods=["POST"])
+def create_player():
+    name = request.form["name"]
+    player_info = request.form["player_info"]
+    goals_scored = request.form["goals_scored"]
+    new_player = Player(name, player_info, goals_scored)
+    player_repository.save(new_player)
+    return redirect("/clubs/players")
+
+
+# EDIT
+@players_blueprint.route("/clubs/players/<id>/edit")
+def edit_player(id):
+    player = player_repository.select(id)
+    return render_template('clubs/players/edit.html', player=player)
+
+
+# UPDATE
+@players_blueprint.route("/clubs/players/<id>", methods=["POST"])
+def update_player(id):
+    name = request.form["name"]
+    player_info = request.form["player_info"]
+    goals_scored = request.form["goals_scored"]
+    player = Player(name, player_info, goals_scored, id)
+    player_repository.update(player)
+    return redirect("/clubs/players")
+
+
+# DELETE
+@players_blueprint.route("/clubs/players/<id>/delete", methods=["POST"])
+def delete_player(id):
+    player_repository.delete(id)
+    return redirect("/clubs/players")
--- repositories/player_repository.py
+++ repositories/player_repository.py
@@ -7,6 +7,7 @@ def save(player):
     results = run_sql(sql, values)
     id = results[0]['id']
     player.id = id
+    return player
 
 
 def select_all():
@@ -19,6 +20,26 @@ def select_all():
     return players
 
 
+def select(id):
+    sql = "SELECT * FROM players WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    player = Player(result["name"], result["player_info"], result["goals_scored"], result["id"])
+    return player
+
 def delete_all():
     sql = "DELETE FROM players"
     run_sql(sql)
+
+
+def delete(id):
+    sql = "DELETE FROM players WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def update(player):
+    sql = "UPDATE players SET (name, player_info, goals_scored) = %s, %s, %s WHERE id = %s"
+    values = [player.name, player.player_info, player.goals_scored, player.id]
+    run_sql(sql, values)
+    return player
